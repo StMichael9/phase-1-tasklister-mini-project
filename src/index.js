fc976dfaade36588f7b5c7bbd186917f0f1c13cb
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-21 18:06:10 -0700

    First Attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const task = event.target["new-task-description"].value;
+
+    buildToDo(task);
+
+    form.reset();
+  });
 });
+
+function buildToDo(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  const list = document.getElementById("tasks");
+  list.appendChild(li);
+}
